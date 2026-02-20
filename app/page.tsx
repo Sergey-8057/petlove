5dc76604cc9b75773dca74444378df6d78941382
Sergey-8057 <erste_sergey@proton.me>
2026-02-20 03:34:02 +0200

    create Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,44 @@
 // import Image from "next/image";
+// import clsx from 'clsx';
+
 import css from './page.module.css';
 
 export default function Home() {
   return (
-    <section className={css.hero}>
-      <div className={css.contTitle}>
-        <h1>Take good care of your small pets</h1>
-        <p>
-          Choosing a pet for your home is a choice that is meant to enrich your life with
-          immeasurable joy and tenderness.
-        </p>
+    <section className={css.container}>
+      <div className={css.hero}>
+        <div className={css.contTitleText}>
+          <h1 className={css.heroTitle}>
+            Take good <span className={css.heroTitleCare}>care</span> of your small pets
+          </h1>
+          <p className={css.heroText}>
+            Choosing a pet for your home is a choice that is meant to enrich your life with
+            immeasurable joy and tenderness.
+          </p>
+        </div>
       </div>
-      <div className={css.contImage}></div>
+      <div className={css.imageWrapper}>
+        <picture>
+          <source
+            media="(min-width: 1280px)"
+            srcSet="image-hero-desk.jpg 1x, image-hero-desk-2x.jpg 2x"
+          />
+          <source
+            media="(min-width: 768px)"
+            srcSet="image-hero-tabl.jpg 1x, image-hero-tabl-2x.jpg 2x"
+          />
+          <source
+            media="(max-width: 767px)"
+            srcSet="image-hero-mob.jpg 1x, image-hero-mob-2x.jpg 2x"
+          />
+          <img
+            className={css.imageHero}
+            src="image-hero-desk.jpg"
+            alt="girl with a dog"
+            width="1216"
+          />
+        </picture>
+      </div>
     </section>
   );
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,54 +2,73 @@
 
 // import { useState } from 'react';
 import Link from 'next/link';
-// import clsx from 'clsx';
-// import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
 
 import css from './Header.module.css';
 
 const Header = () => {
-  // const pathname = usePathname();
+  const pathname = usePathname();
 
   return (
-    <header className={css.header}>
-      {/* Logo */}
-      <Link className={css.logo} href="/" aria-label="Home">
-        petl
-        <svg className={css.iconHeart} width="23" height="23" aria-hidden="true">
-          <use href="/symbol-defs.svg#icon-heart" />
-        </svg>
-        ve
-      </Link>
+    <section className={css.container}>
+      <header className={css.header}>
+        {/* Logo */}
+        <Link className={css.logo} href="/" aria-label="Home">
+          petl
+          <svg className={css.iconHeart} width="23" height="23" aria-hidden="true">
+            <use href="/symbol-defs.svg#icon-heart" />
+          </svg>
+          ve
+        </Link>
 
-      {/* Navigation */}
-      <nav>
-        <ul className={css.navigation}>
-          <li>
-            {/* <Link
-              href="/"
-              className={clsx(css.navLink, {
-                [css.isActive]: pathname === '/',
-              })}
-            >
-              Home
-            </Link> */}
-          </li>
+        {/* Navigation */}
+        <nav className={css.navigation}>
+          <ul className={css.listNavigation}>
+            <li>
+              <Link
+                href="/news"
+                className={clsx(css.navLink, {
+                  [css.isActive]: pathname === '/news',
+                })}
+              >
+                News
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/notices"
+                className={clsx(css.navLink, {
+                  [css.isActive]: pathname === '/notices',
+                })}
+              >
+                Find pet
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/friends"
+                className={clsx(css.navLink, {
+                  [css.isActive]: pathname === '/friends',
+                })}
+              >
+                Our friends
+              </Link>
+            </li>
 
-          <li>
-            {/* <Link
-              href="/teachers"
-              className={clsx(css.navLink, {
-                [css.isActive]: pathname === '/teachers',
-              })}
-            >
-              Teachers
-            </Link> */}
-          </li>
-
-          {/* ✅ Favorites — only for authorized users */}
-        </ul>
-      </nav>
-    </header>
+            {/* Auth */}
+          </ul>
+          <div className={css.listAuth}>
+            <Link href="/login" className={css.linkLogin}>
+              Log In
+            </Link>
+            <Link href="/registr" className={css.linkRegistr}>
+              Registration
+            </Link>
+          </div>
+        </nav>
+      </header>
+    </section>
   );
 };
 
